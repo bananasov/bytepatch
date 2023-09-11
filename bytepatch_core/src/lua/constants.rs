ebffb49693b95e2e4e250aa8731bbb54e9cb6b27
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2023-09-11 08:47:24 +0000

    start implementing constant decoding

--- bytepatch_core/src/lua/constants.rs
+++ bytepatch_core/src/lua/constants.rs
@@ -1,3 +1,5 @@
+use scroll::Endian;
+
 use super::LuaString;
 
 #[derive(Debug)]
@@ -7,4 +9,14 @@ pub enum Constant {
     LUA_TBOOLEAN(bool),
     LUA_TNUMBER(u32),
     LUA_TSTRING(LuaString),
+}
+
+impl<'a> Constant {
+    pub fn decode(
+        src: &'a [u8],
+        offset: &mut usize,
+        endian: Endian,
+    ) -> Result<Constant, Box<dyn std::error::Error>> {
+        todo!()
+    }
 }
