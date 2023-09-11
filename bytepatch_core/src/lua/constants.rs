a64a5ca42148b2d4b1ef5bb193a453906707fcc6
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2023-09-11 09:19:35 +0000

    not a clue if this works or not, do later

--- bytepatch_core/src/lua/constants.rs
+++ bytepatch_core/src/lua/constants.rs
@@ -1,4 +1,4 @@
-use scroll::Endian;
+use scroll::{Endian, Pread};
 
 use super::LuaString;
 
@@ -15,8 +15,32 @@ impl<'a> Constant {
     pub fn decode(
         src: &'a [u8],
         offset: &mut usize,
+        size_t_size: u8,
         endian: Endian,
     ) -> Result<Constant, Box<dyn std::error::Error>> {
-        todo!()
+        let const_type: u8 = src.gread_with(offset, endian)?;
+        let constant = match const_type {
+            0 => Constant::LUA_TNIL,
+            1 => {
+                let value: u8 = src.gread_with(offset, endian)?;
+                Constant::LUA_TBOOLEAN(value != 0)
+            },
+            3 => {
+                let value: u32 = src.gread_with(offset, endian)?;
+                Constant::LUA_TNUMBER(value)
+            },
+            4 => {
+                let str = match size_t_size {
+                    4 => LuaString::read_u32(src, offset, endian)?,
+                    8 => LuaString::read_u64(src, offset, endian)?,
+                    _ => unreachable!(),
+                };
+
+                Constant::LUA_TSTRING(str)
+            },
+            _ => unreachable!("Somehow got an invalid constant type")
+        };
+
+        Ok(constant)
     }
 }
